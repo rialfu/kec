4fe33748df81401ff10035526a5ba61e4226f989
Fadhel Murphy <fadhelijlalfalah@gmail.com>
2019-03-17 08:25:06 +0700

    update lagi

--- lare/database/migrations/2019_03_02_144127_tabel_penduduk.php
+++ lare/database/migrations/2019_03_02_144127_tabel_penduduk.php
@@ -15,6 +15,21 @@ class TabelPenduduk extends Migration
     {
         Schema::create('penduduk', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nama');
+            $table->string('nik');
+            $table->string('kk');
+            $table->string('tempatlahir');
+            $table->date('ttl');
+            $table->string('jk');
+            $table->string('goldar');
+            $table->string('agama');
+            $table->string('alamat');
+            $table->string('perkawinan');
+            $table->string('warga');
+            $table->string('pekerjaan');
+            $table->string('ayah');
+            $table->string('ibu');
+            $table->string('foto')->nullable();
             $table->timestamps();
         });
     }
